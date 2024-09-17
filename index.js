d2707ed0a8fad9fd78404a4d2534d22f658f2373
YashGupta404 <yashgupta.work.2005@gmail.com>
2024-09-18 03:03:42 +0530

    Increased Authentication using Bcrypt Hashing and salting

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";  
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -25,6 +27,7 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
@@ -43,12 +46,23 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("Thanks for registering to our website!");
+
+// Password Hashing using Bcrypt and salting
+bcrypt.hash(password,saltRounds, async (err,hash)=>{
+  if(err){
+    console.log("Error Hashing Problem : ",err);
+  }
+  else{
+  const result = await db.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2)",
+    [email, hash]
+  );
+  console.log(result);
+  res.send("Thanks for registering to our website!");
+}
+})
+
+      
     }
   } catch (err) {
     console.log(err);
@@ -57,7 +71,7 @@ app.post("/register", async (req, res) => {
 // Login User
 app.post("/login", async (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const loginpassword = req.body.password;
 
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
@@ -66,13 +80,23 @@ app.post("/login", async (req, res) => {
     //Check if email is registered
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = user.password;
+      const storedHashedPassword = user.password;
 
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
+      // Use this to compare hashed passwords, here result is True or False depending on the password entered is correct or not
+      bcrypt.compare(loginpassword,storedHashedPassword, (err,result)=>{
+        if(err){
+          console.log("Error comparing passwords: ",err);
+        }
+        else{
+          if(result){
+            res.render("secrets.ejs");
+          }
+          else{
+            res.send("Incorrect Password"); 
+          }
+        }
+      })
+      
     } else {
       res.send("User not found");
     }
